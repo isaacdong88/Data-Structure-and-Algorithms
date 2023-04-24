b3c7b33ef1faf7919672fcc6ccb900aad114723e
isaacdong88 <isaacdong88@gmail.com>
2023-04-24 14:36:05 -0400

    linear search

---
+++ Algorithms/linearSearch.js
@@ -0,0 +1,17 @@
+//Linear search function takes in an array and an value return index if value is in array
+//returns -1 if not
+const linearSearch = (arr, value) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return `${value} is found at index of ${i}`;
+    }
+  }
+  return -1;
+};
+
+console.log(linearSearch([0, 1, 1, 2, 3], 4));
+
+//This is indexOf, findIndex, includes behind the scene is doing
+
+//Big O
+//O(n) as input increase so does the time also increase
